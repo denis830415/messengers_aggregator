de3d4c41d026b5717c97c87566c75cbdfe14ce99
denis830415 <denis830415@gmail.com>
2025-12-18 16:36:19 +0300

    Create settings.py

---
+++ app/config/settings.py
@@ -0,0 +1,11 @@
+"""
+Пользовательские настройки приложения.
+"""
+
+# TODO: хранение настроек
+# TODO: загрузка/сохранение
+
+DEFAULT_SETTINGS = {
+    "enabled_messengers": ["telegram", "whatsapp", "viber"],
+    "history_days": 30,
+}
