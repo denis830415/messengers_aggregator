f2d2a933513d6bd88679eb37aa7ad3b7826bc167
denis830415 <denis830415@gmail.com>
2025-12-18 16:37:34 +0300

    Create NotificationListener.java

---
+++ java/NotificationListener.java
@@ -0,0 +1,20 @@
+/*
+ * Android Notification Listener Service
+ *
+ * Ответственность:
+ * - Перехват системных уведомлений
+ * - Передача данных в Python через pyjnius
+ */
+
+package org.messenger.aggregator;
+
+// TODO: импортировать необходимые Android классы
+
+public class NotificationListener {
+
+    // TODO: наследование от NotificationListenerService
+
+    // TODO: onNotificationPosted()
+
+    // TODO: передача данных в Python
+}
