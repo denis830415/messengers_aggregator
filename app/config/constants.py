20b1683878e935c53f604fcba0ef0bd9033bda0a
denis830415 <denis830415@gmail.com>
2025-12-18 16:36:59 +0300

    Create constants.py

---
+++ app/config/constants.py
@@ -0,0 +1,11 @@
+"""
+Константы проекта.
+"""
+
+MESSENGERS = {
+    "telegram": "org.telegram.messenger",
+    "whatsapp": "com.whatsapp",
+    "viber": "com.viber.voip",
+}
+
+# TODO: иконки, цвета, приоритеты
