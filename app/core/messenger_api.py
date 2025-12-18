e856ea66dc78131b76eaa66756986236ccf2ce2c
denis830415 <denis830415@gmail.com>
2025-12-18 16:33:54 +0300

    Create messenger_api.py

---
+++ app/core/messenger_api.py
@@ -0,0 +1,22 @@
+"""
+Отправка сообщений через Android API.
+
+Методы:
+- Ответ через Direct Reply
+- Отправка через Intent
+"""
+
+# TODO: импортировать pyjnius и Android классы
+
+class MessengerAPI:
+    """API для отправки сообщений"""
+
+    def reply(self, message_id, text):
+        """Ответить на сообщение"""
+        # TODO: реализовать RemoteInput
+        pass
+
+    def send_new(self, messenger, contact, text):
+        """Отправить новое сообщение"""
+        # TODO: сформировать Intent
+        pass
